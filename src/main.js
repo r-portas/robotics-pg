c3283d47ede856ba68ba39fe7736b1045495d55d
Roy Portas <royportas@gmail.com>
2017-12-05 20:35:36 +1000

    Added Google Analytics

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import App from './App'
 import router from './router'
 
 import './main.scss'
 import 'spinkit/css/spinkit.css'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-79288806-5',
+  router
+})
 
 Vue.config.productionTip = false
 
