f9494eae6c6cf023331d2c74603f3d51663655d0
Wilson Kong <wilson.kong@uqconnect.edu.au>
2018-01-03 19:50:32 +1000

    Mailing list.

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -2,6 +2,21 @@
   <footer class="footer">
     <div class="container">
       <div class="content has-text-centered">
+        <!-- Begin MailChimp Signup Form -->
+        <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+        <div id="mc_embed_signup">
+        <form action="https://RoboticsPG.us17.list-manage.com/subscribe/post?u=8d1b50dd9e85a230343686633&amp;id=21d3d9b77d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+          <label for="mce-EMAIL">Subscribe to our mailing list</label>
+          <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8d1b50dd9e85a230343686633_21d3d9b77d" tabindex="-1" value=""></div>
+            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+            </div>
+        </form>
+        </div>
+
+        <!--End mc_embed_signup-->
         <p><strong>Robotics Playground</strong> is a startup who is bringing robotics to everyone!</p>
         <p>FB: <a href="https://fb.me/RoboticsPG">fb.me/RoboticsPG</a> | E: 
         <a href="mailto:roboticspg@gmail.com" target="_top">RoboticsPG@gmail.com</a> | T: <a href="https://twitter.com/RoboticsPG">@RoboticsPG</a>
